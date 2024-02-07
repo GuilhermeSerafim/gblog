718298f23b5932e5404c9f8a88f51062654c354c
GuilhermeSerafim <guilerstudies@gmail.com>
2024-02-07 14:48:52 -0300

    Implementação de rotas com router-dom e tratamento de página não encontrada

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Inicio from './componentes/Inicio';
+import SobreMim from './componentes/SobreMim';
 
 function App() {
   return (
     <div className="App">
-    <h1>GBLOG</h1>
+      <BrowserRouter> {/* Criando SPA */}
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/sobremim" element={<SobreMim />} />
+          <Route path="/*" element={<h1>Página não encontrada</h1>} /> 
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
