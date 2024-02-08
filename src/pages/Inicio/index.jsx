934afb4fc5e98b248ecad31d17ad8789429ac64e
GuilhermeSerafim <guilerstudies@gmail.com>
2024-02-08 14:36:58 -0300

    Configurando imports absolutos no projeto e aplicando ao componente Banner

--- src/pages/Inicio/index.jsx
+++ src/pages/Inicio/index.jsx
@@ -1,5 +1,10 @@
+import Banner from "componentes/Banner";
+
 export default function Inicio() {
     return (
+        <main>
+            <Banner/>
         <h1>Gblog</h1>
+        </main>
     )
-}
+}   
