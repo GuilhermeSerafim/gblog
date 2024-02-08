385a65fddb2a665cee6851314c1affb0c5071641
GuilhermeSerafim <guilerstudies@gmail.com>
2024-02-08 10:34:50 -0300

    Implementando modularização de css, e navegação entre as pages pelo menu

--- src/componentes/Menu/index.jsx
+++ src/componentes/Menu/index.jsx
@@ -1,5 +1,12 @@
 import styles from "./Menu.module.css";
 
 export default function Menu() {
-    return "Menu de navegação"
+    return (
+        <header>
+            <nav className={styles.navegacao}>
+                <a href="/" className={styles.link}>Inicio</a>
+                <a href="/sobremim" className={styles.link}>Sobre Mim</a>
+            </nav>
+        </header>
+    )
 }
