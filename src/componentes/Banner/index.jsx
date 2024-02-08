513345f7bc01684256a44f104d3915a2c2b35f97
GuilhermeSerafim <guilerstudies@gmail.com>
2024-02-08 15:13:42 -0300

    Estilizando banner

--- src/componentes/Banner/index.jsx
+++ src/componentes/Banner/index.jsx
@@ -1,7 +1,31 @@
 import styles from "./Banner.module.css";
-
+import circuloColorido from "assets/circulo_colorido.png";
 export default function Banner() {
     return (
-        "Banner"
+        <div className={styles.banner}>
+            <div className={styles.apresentacao}>
+                <h1 className={styles.titulo}>
+                    Bem-vindo Explorador
+                </h1>
+                <p className={styles.paragrafo}>
+                    Pronto para uma jornada emocionante pelos horizontes infinitos da tecnologia? Neste blog, você será como um verdadeiro explorador, desbravando os territórios desconhecidos da inteligência artificial, mergulhando nas profundezas da mente humana e contemplando os mistérios do universo.
+                    Prepare-se para uma viagem além da imaginação, onde cada descoberta é uma porta para o futuro.
+                </p>
+            </div>
+            <div className={styles.imagens}>
+                <img
+                    className={styles.circuloColorido}
+                    src={circuloColorido}
+                    alt="Circulo colorido"
+                    aria-hidden={true}
+                />
+                <img
+                    className={styles.minhaFoto}
+                    src="https://static.vecteezy.com/ti/fotos-gratis/p2/23574317-astronauta-gato-dentro-espaco-capacete-retrato-do-uma-gato-ai-generativo-imagem-gratis-foto.jpg"
+                    alt="Minha foto"
+                    aria-hidden={true}
+                />
+            </div>
+        </div>
     )
 }
