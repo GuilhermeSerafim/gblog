6b06fca2530275c8e65089f639a35fb1ab5b0d0e
GuilhermeSerafim <guilerstudies@gmail.com>
2024-02-15 14:51:16 -0300

    Instalando material ui, e estilizando menu

--- src/componentes/Menu/index.jsx
+++ src/componentes/Menu/index.jsx
@@ -1,13 +1,17 @@
 import styles from "./Menu.module.css";
 import MenuLink from "../MenuLink";
+import { Button } from "@mui/material";
 
 export default function Menu() {
 
     return (
         <header>
             <nav className={styles.navegacao}>
-                <MenuLink to={"/"}>Ínicio</MenuLink>
-                <MenuLink to={"/sobremim"}>Sobre Mim</MenuLink>
+                <MenuLink to={"/"}>ÍNICIO</MenuLink>
+                <MenuLink to={"/sobremim"}>SOBRE MIM</MenuLink>
+                <Button color="secondary" variant="outlined">
+                    <MenuLink to={"/#"}>NEWSLETTER</MenuLink> {/*Em desenvolvimento */}
+                </Button>
             </nav>
         </header>
     )
