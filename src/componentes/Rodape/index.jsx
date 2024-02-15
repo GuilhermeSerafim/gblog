7fc8f90b303e220949449eaf89f774a3b46cc942
GuilhermeSerafim <guilerstudies@gmail.com>
2024-02-15 15:05:43 -0300

    Finalizando o rodapé

--- src/componentes/Rodape/index.jsx
+++ src/componentes/Rodape/index.jsx
@@ -4,6 +4,23 @@ import { ReactComponent as MarcaRegistrada } from "assets/marca_registrada.svg";
 export default function Rodape() {
     return (
         <footer className={styles.rodape}>
+            <ul className={styles.logos}>
+                    <li>
+                        <a href="https://www.youtube.com/channel/UCDwZfT1FeLccSRAgWubJHng" target="blank">
+                            <img src="assets/youtube.png" alt="Youtube" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="wa.link/8dnjl7" target="blank">
+                            <img src="assets/whatsapp.png" alt="Whatsapp" />
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/iamguiler/" target="blank">
+                            <img src="assets/ig.png" alt="Instagram" />
+                        </a>
+                    </li>
+                </ul>
             <MarcaRegistrada />
             <p>
                 Desenvolvido por <a className={styles.rodape__autor} href="https://guilhermeserafim.github.io/portifolio/" target='blank'>Guilherme</a>
