0b34c194126a22fa9408cc732519f6cfa5c9a253
GuilhermeSerafim <guilerstudies@gmail.com>
2024-02-21 14:13:40 -0300

    Deixando menu responsivo

--- src/componentes/MenuLink/index.jsx
+++ src/componentes/MenuLink/index.jsx
@@ -4,15 +4,17 @@ import styles from './MenuLink.module.css';
 export default function MenuLink({ children, to }) {
 
     return (
-        <NavLink
-            className={({ isActive }) => `
+        <div className={styles.container}>
+            <NavLink
+                className={({ isActive }) => `
                 ${styles.link}
                 ${isActive ? styles.linkDestacado : ""}
             `}
-            to={to}
-            end
-        >   
-            {children}
-        </NavLink>
+                to={to}
+                end
+            >
+                {children}
+            </NavLink>
+        </div>
     )
 }
