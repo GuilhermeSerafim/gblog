89be7ff48e172040ec63004cfb4f7aecc11da0b1
GuilhermeSerafim <guilerstudies@gmail.com>
2024-02-20 23:50:13 -0300

    Mudando texto sobre mim

--- src/paginas/SobreMim/index.jsx
+++ src/paginas/SobreMim/index.jsx
@@ -19,13 +19,13 @@ export default function SobreMim() {
 
 
             <p className={styles.paragrafo}>
-                Me apresento inicialmente como programador, minha história com programação, começou em 2020 quando entrei para Etec Abdias do Nascimento. Desde muito jovem meu fascínio pela exploração do desconhecido me levou a mergulhar de cabeça no universo da programação e outras áreas do meu interesse.
+                Me apresento inicialmente como programador, minha história com programação, começou em 2020 quando entrei para ETEC (Escola Técnica Estadual). Desde muito jovem meu fascínio pela exploração do desconhecido me levou a mergulhar de cabeça no universo da programação e outras áreas do meu interesse.
             </p>
             <p className={styles.paragrafo}>
                 Na pandemia, sem acesso a um computador, aprendendo no formato EAD, passei três anos lutando para aprender o básico de programação. Imagine a frustração! Mas como um verdadeiro explorador destemido, recusei-me a desistir. Com determinação, consegui um emprego como vendedor de videogames e, com o dinheiro economizado, comprei meu próprio computador. Em pouco tempo, consegui uma posição como programador na renomada seguradora Tokio Marine.
             </p>
             <p className={styles.paragrafo}>
-                Minha jornada de aprendizado não parou por aí. Aprendi muito na prática, no dia a dia do trabalho, e também na maior escola de tecnologia da América Latina: a Alura. Agora, munido das ferramentas necessárias, estou em constante evolução e aprendizado. Uma frase que ecoou profundamente em mim durante todo esse processo foi a de Mário Sérgio Cortella: "Faça o teu melhor na condição que você tem, enquanto você não tem condições melhores para fazer melhor ainda!"            </p>
+                Minha jornada de aprendizado não parou por aí. Aprendi muito na prática, no dia a dia do trabalho, e também na maior escola de tecnologia da América Latina: a Alura. Agora, munido das ferramentas necessárias, estou em constante evolução e aprendizado. Uma frase que ecoou profundamente em mim durante todo esse processo foi a frase de Sérgio Cortella: "Faça o teu melhor na condição que você tem, enquanto você não tem condições melhores para fazer melhor ainda!"            </p>
             <p className={styles.paragrafo}>
                 Mas não me rótulo apenas um programador; também há o Guilherme armador do basquete; o Guiler dos jogos; Entre outras versões de mim mesmo.  Encontrar o equilíbrio entre minhas diferentes identidades é complexo, mas entendo isso como uma riqueza, que me permite explorar plenamente todas as dimensões do meu ser.
             </p>
